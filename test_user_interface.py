e5b643bdc6f0e2e670c54d39ef8df5ec20adfa12
Luca Leyva <imlucaleyva@gmail.com>
2021-10-14 16:57:04 -0400

    Created test cases for get_unique_can_names

--- test_user_interface.py
+++ test_user_interface.py
@@ -1,5 +1,8 @@
 import unittest
 import user_interface
+from cans import Cola
+from cans import OrangeSoda
+from cans import RootBeer
 
 class TestValidateMainMenue(unittest.TestCase):
     """Pass in each number 1-4, ensure the tuple of (True, number) is returned
@@ -30,6 +33,32 @@ class TestTryParseInt(unittest.TestCase):
          """Pass in hello and ensure 0 is returned"""
          hello = user_interface.try_parse_int("hello")
          self.assertEqual(hello, 0)
+
+class TestGetUniqueCanNames(unittest.TestCase):
+   """a. Instantiate 6 cans (two of each type) and append them to a list. Pass the list into this
+function, ensure the returned list only contains 3 names
+b. Pass in an empty list. Ensure an empty list is returned."""
+   def test_get_unique_can_names(self):
+      self.can_list = []
+      cola_one = Cola()
+      cola_two = Cola()
+      orange_soda_one = OrangeSoda()
+      orange_soda_two = OrangeSoda()
+      root_beer_one = RootBeer()
+      root_beer_two = RootBeer()
+      self.can_list.append(cola_one)
+      self.can_list.append(cola_two)
+      self.can_list.append(orange_soda_one)
+      self.can_list.append(orange_soda_two)
+      self.can_list.append(root_beer_one)
+      self.can_list.append(root_beer_two)
+      cans = user_interface.get_unique_can_names(self.can_list)
+      self.assertEqual(len(cans),3)
+   def test_get_empty_can_list(self):
+      self.empty_can_list = []
+      cans = user_interface.get_unique_can_names(self.empty_can_list)
+      self.assertEqual(len(cans), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
