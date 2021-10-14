7f6a17242e8df32b4638f1211a44b770e93c4d0a
Luca Leyva <imlucaleyva@gmail.com>
2021-10-14 11:31:36 -0400

    Added get_coin_from_register test case for penny.

--- test_soda_machine.py
+++ test_soda_machine.py
@@ -1,10 +1,12 @@
 import unittest
 from soda_machine import SodaMachine
+from coins import Penny
+from coins import Nickel
+from coins import Dime
+from coins import Quarter
 
-class TestFill_Register(unittest.TestCase):
-    """
-    Instantiate a SodaMachine object, test that its register list has a len of 88
-    """
+class TestFillRegister(unittest.TestCase):
+    """Instantiate a SodaMachine object, test that its register list has a len of 88"""
     def setUp(self):
         self.soda_machine = SodaMachine()
     
@@ -12,10 +14,22 @@ class TestFill_Register(unittest.TestCase):
         test_var = self.soda_machine.register
         self.assertEqual(len(test_var),88)
    
+class TestFillInventory(unittest.TestCase):
     """Testing to see if invetory list has a length of 30"""
+    def setUp(self):
+        self.soda_machine = SodaMachine()
     def test_fill_inventory(self):
         test_var = self.soda_machine.inventory
         self.assertEqual(len(test_var), 30)
+
+class TestGetCoinFromRegister(unittest.TestCase):
+    """Test each type of coin can be returned from register"""
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+    def test_get_coin_from_register(self):
+        returned_penny = self.soda_machine.get_coin_from_register("Penny")
+        self.assertTrue(returned_penny)
 
 if __name__ == '__main__':
     unittest.main()
